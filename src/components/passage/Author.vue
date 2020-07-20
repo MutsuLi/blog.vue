557ee8d20806c7a0d488a84337fee594db31f60e
okitasann <alucard_invidia@hotmail.com>
2020-07-20 23:38:23 +0800

    update

--- src/components/passage/Author.vue
+++ src/components/passage/Author.vue
@@ -1,18 +1,16 @@
  <template>
   <v-card class="mx-auto" max-width="344" outlined>
     <v-list-item>
-      <v-list-item-avatar color="grey"></v-list-item-avatar>
+      <v-list-item-avatar color="grey" size="48"></v-list-item-avatar>
       <v-list-item-content>
-        <v-list-item-title class="headline">Our Changing Planet</v-list-item-title>
-        <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle>
+        <v-list-item-title class="headline">{{author.name}}</v-list-item-title>
+        <v-list-item-subtitle>{{author.title}}</v-list-item-subtitle>
       </v-list-item-content>
     </v-list-item>
     <v-card-text>
-      <p>adjective</p>
-      <div class="text--primary">
-        well meaning and kindly.
-        <br />"a benevolent smile"
-      </div>
+      <v-row> </v-row>
+      <p>文章: {{author.passagesCount}}</p>
+      <div class="text--primary">{{author.info}}</div>
     </v-card-text>
     <v-card-actions>
       <v-btn text color="deep-purple accent-4">Learn More</v-btn>
@@ -21,6 +19,15 @@
 </template>
 <script>
 export default {
-  name: "PassageAuthor"
+  name: "PassageAuthor",
+  data: () => ({
+    author: {
+      name: "mutsuli",
+      title: "soft engineer",
+      info: "someone like you",
+      passagesCount: 10
+    },
+    ReleaseTime: "2020-07-20"
+  })
 };
 </script>
