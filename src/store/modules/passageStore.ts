3a7280eeb99e9bd765a947f3c2718372f001d870
ricardo <alucard_invidia@hotmail.com>
2020-07-20 14:34:21 +0800

    fix

--- src/store/modules/passageStore.ts
+++ src/store/modules/passageStore.ts
@@ -103,23 +103,39 @@ const mutations = {
     [TYPE.MENU_SUCCESS](state, response) {
         state.menus = response;
     },
-    [TYPE.CONTENT_PASSAGE_SUCCESS](state, response) {
-        let passage = response;
-        let result = {
-            ID: passage.bID,
-            category: passage.bcategory,
-            title: passage.btitle,
-            content: passage.bcontent,
-            createTime: passage.bCreateTime,
-            updateTime: passage.bUpdateTime,
-            submitter: passage.bsubmitter,
-            remark: passage.bRemark,
-            next: passage.next,
-            nextID: passage.nextID,
-            previousID: passage.previousID,
-            previous: passage.previous
-        };
-        state.passage = result;
+     [TYPE.CONTENT_REQUEST](state) {
+
+    },
+    [TYPE.CONTENT_SUCCESS](state, response) {
+        let list = response.data;
+        for (let i = 0; i < list.length; i++) {
+            let passage = list[i]
+            let rowItem = {
+                ID: passage.bID,
+                category: passage.bcategory,
+                title: passage.btitle,
+                content: passage.bcontent,
+                createTime: passage.bCreateTime,
+                updateTime: passage.bUpdateTime,
+                submitter: passage.bsubmitter,
+                remark: passage.bRemark,
+                href: passage.href
+            }
+            state.passages.push(rowItem)
+        }
+        // for(let key of state.sortKeys) {
+        // 	// console.log(JSON.stringify(Object.values(response[key])))
+        // 	let rowItem = {
+        // 		categoty: 0,
+        // 		key: response[key],
+        // 		data: Object.values(response[key])
+        // 	}
+        // 	// state.rows.push(rowItem)
+        // 	state.rows.push(Object.values(response[key]))
+        // }
+    },
+    [TYPE.CONTENT_FAILURE](state) {
+
     },
     // 排行榜信息
     [TYPE.CONTENT_RANK_REQUEST](state) {
