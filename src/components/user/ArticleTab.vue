fa68799b0767ef0591040b22084a0817dc1dcad6
okitasann <alucard_invidia@hotmail.com>
2020-07-26 20:46:56 +0800

    update

--- src/components/user/ArticleTab.vue
+++ src/components/user/ArticleTab.vue
@@ -1,19 +1,7 @@
 <template>
   <v-container>
-    <v-card color="primary">
-      <v-tabs v-model="tab">
-        <v-tab>Edit</v-tab>
-        <v-tab>Preview</v-tab>
-      </v-tabs>
-
-      <v-tabs-items v-model="tab">
-        <v-tab-item>
-          <user-editor></user-editor>
-        </v-tab-item>
-        <v-tab-item>
-          <user-preview />
-        </v-tab-item>
-      </v-tabs-items>
+    <v-card>
+      <user-editor></user-editor>
     </v-card>
   </v-container>
 </template>
@@ -34,11 +22,5 @@ export default {
 };
 </script>
 
-  <style>
-.basil {
-  background-color: #fffbe6 !important;
-}
-.basil--text {
-  color: #356859 !important;
-}
-</style>>
+<style>
+</style>
--- src/components/user/Editor.vue
+++ src/components/user/Editor.vue
@@ -1,21 +1,25 @@
 <template>
-  <v-container>
+  <v-container class="mx-auto">
     <v-text-field v-model="title" :rules="titleRules" label="title" required outlined rounded></v-text-field>
-    <v-textarea
-      name="input-7-1"
-      filled
-      auto-grow
-      value="1The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
-    ></v-textarea>
+    <v-text-field v-model="tag" :rules="tagRules" label="tag" required outlined rounded></v-text-field>
+    <mavon-editor ref="editor" v-model="doc" style="min-height: 800px"></mavon-editor>
   </v-container>
 </template>
 
 <script>
+import { mavonEditor } from "mavon-editor";
+import "mavon-editor/dist/css/index.css";
 export default {
   name: "userEditor",
-  components: {},
+  components: { mavonEditor },
   data: () => ({
     title: "",
+    tag: "",
+    doc: "",
+    tagRules: [
+      (v) => !!v || "tag is required",
+      (v) => (v && v.length <= 10) || "tag must be less than 10 characters",
+    ],
     titleRules: [
       (v) => !!v || "title is required",
       (v) => (v && v.length <= 10) || "title must be less than 10 characters",
